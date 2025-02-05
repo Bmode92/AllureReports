ca99d46a96862b4861e33c8e9ada500cd9b872f7
Ioan <sioan@luxoft.com>
2025-02-05 12:03:59 +0200

    done

---
+++ src/test/java/com/example/tests/FlakyTest.java
@@ -0,0 +1,13 @@
+package com.example.tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FlakyTest {
+
+    @Test
+    @DisplayName("Flaky Test Example")
+    public void testFlaky() {
+
+    }
+}
